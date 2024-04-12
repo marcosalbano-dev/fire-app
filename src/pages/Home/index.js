6e4ab5ff8e063944544958438bf8f81d80790c90
Marcos Albano <marcos.telematica@gmail.comgit>
2024-04-12 15:25:00 -0300

    Add página home.css e index.js(Home)

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,50 @@
-﻿export default function Home(){
+﻿import { useState } from "react"
+import './home.css'
+import { Link } from "react-router-dom";
+
+export default function Home(){
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleLogin(e){
+      e.preventDefault();
+
+      if(email !== '' && password !== ''){
+        alert('teste')
+      }else{
+        alert('Preencha todos os campos!')
+      }
+
+      
+    }
+
     return(
-      <div>
-        <h1>Página Home</h1>
+      <div className="home-container">
+        <h1>Lista de Tarefas</h1>
+        <span>Gerencie sua agenda de forma fácil.</span>
+
+        <form className="form" onSubmit={handleLogin}>
+            <input 
+                type="text"
+                placeholder="Digite seu email..."
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <input 
+                type="password"
+                placeholder="*********"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <button type="submit">Acessar</button>
+        </form>
+
+        <Link className="button-link" to="/register">
+          Não possui conta? Cadastre-se.
+        </Link>
       </div>
     )
   }
